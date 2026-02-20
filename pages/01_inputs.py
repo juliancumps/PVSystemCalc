3c1bd6898ad185b2bda282c220f66651f2bc8e87
Julian Cumps <jwtcumps@gmail.com>
2026-02-20 13:07:05 -0900

    Add input fields for solar panel and battery system info

---
+++ pages/01_inputs.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+st.header("Solar Panel Info")
+st.subheader("Input Information From Your Solar Panel Datasheet")
+st.session_state.ppp = st.number_input("Power Per Panel (Watts)", min_value=0.0, value=410.00)
+st.session_state.voc = st.number_input("Open Circuit Voltage (Voc) in Volts)", min_value=0.0, value=37.41)
+st.session_state.isc = st.number_input("Short Circuit Current (Isc) in Amps)", min_value=0.0, value=13.90)
+st.session_state.vmp = st.number_input("Voltage @ Max Power Point (Vmp) in Volts)", min_value=0.0, value=31.38)
+st.session_state.imp = st.number_input("Current @ Max Power Point (Imp) in Amps)", min_value=0.0, value=13.07)
+st.session_state.tempCoeff = st.number_input(
+    "Temperature Coefficient of Voc (%/°C)", 
+    min_value=-1.0, 
+    value=-0.254,
+    step=0.001,
+    help="Enter the percentage value (e.g., 0.324 for 0.324%/°C)",
+    format="%.3f"
+)
+
+st.header("Solar Charger Info")
+st.subheader("Input Information From Your MPPT Charge Controller Datasheet")
+st.session_state.maxV = st.number_input("Max PV Voltage", min_value=0.0, value=150.0)
+st.session_state.macA = st.number_input("Max Charge Current (Amps)", min_value=0.0, value=60.0)
+st.session_state.nominalpv12 = st.number_input("Nominal PV Power in Watts for 12V", min_value=0.0, value=860.0)
+st.session_state.nominalpv24 = st.number_input("Nominal PV Power in Watts for 24V", min_value=0.0, value=2200.0)
+st.session_state.nominalpv48 = st.number_input("Nominal PV Power in Watts for 48V", min_value=0.0, value=3440.0)
+st.session_state.tcold = st.number_input("Coldest Temperature That Your System May Encounter (°F)", min_value=-80.0, value=-10.0)
+st.session_state.stc = 25 #this is in degrees C, but tcold is in F so we must convert at some point.
+
+
+st.header("Battery System Info")
+st.subheader("Input Information About Your Battery Bank")
+st.session_state.battery_voltage = st.number_input("Battery Bank Nominal Voltage (12V, 24V or 48V)", min_value=0.0, value=48.0)
