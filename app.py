aa5a8d3d6af57766d28f85348047f0816a1b6227
Julian Cumps <jwtcumps@gmail.com>
2026-02-20 13:05:41 -0900

    init commit

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Solar Panel Calculator", layout="wide")
+
+st.title("PV System Calculator")
+st.write("Use the sidebar to navigate between pages.")
