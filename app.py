822d3ed705555e489c59f1ba1959b1d7ee88065e
Julian Cumps <jwtcumps@gmail.com>
2026-02-23 09:04:35 -0900

    testing CLI

--- app.py
+++ app.py
@@ -4,3 +4,4 @@ st.set_page_config(page_title="Solar Panel Calculator", layout="wide")
 
 st.title("PV System Calculator")
 st.write("Use the sidebar to navigate between pages.")
+st.write("Currently set up for determining safe configurations of solar panels for MPPT charge controller.")
